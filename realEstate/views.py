58d645779725b509ed96eae23281331b14f7d1ba
egilltor17 <egilltor17@ru.is>
2019-05-08 11:00:00 +0000

    Atributes added to the property details page

--- realEstate/views.py
+++ realEstate/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.http import HttpResponse  # legacy
-from realEstate.models import Property
+from realEstate.models import Property, PropertyAttribute, Attribute
 
 # Create your views here.
 
@@ -12,5 +12,7 @@ def index(request):
 
 def property_details(request, id):
     return render(request, 'realEstate/property_details.html', {
-        'property': get_object_or_404(Property, pk=id)
+        'property': get_object_or_404(Property, pk=id),
+        'propertyAttributes': get_list_or_404(PropertyAttribute, property_id=id),
+        'attributes': Attribute.objects.order_by('description')
     })
