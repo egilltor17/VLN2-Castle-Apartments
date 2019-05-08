13a794b682946157e1c62e219d78584a61b6e516
egilltor17 <egilltor17@ru.is>
2019-05-08 11:17:16 +0000

    Added atributes to details and refactored filters into its own html file

--- realEstate/views.py
+++ realEstate/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from django.http import HttpResponse  # legacy
 from realEstate.models import Property, PropertyAttribute, Attribute
 
 # Create your views here.
@@ -13,6 +12,6 @@ def index(request):
 def property_details(request, id):
     return render(request, 'realEstate/property_details.html', {
         'property': get_object_or_404(Property, pk=id),
-        'propertyAttributes': get_list_or_404(PropertyAttribute, property_id=id),
+        'propertyAttributes': PropertyAttribute.objects.filter(property_id=id),
         'attributes': Attribute.objects.order_by('description')
     })
