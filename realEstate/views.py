96d1cf37307acdd29b29396e16d08b7ff6231490
Haraldur <haraldurloki@gmail.com>
2019-05-10 17:43:54 +0000

    FILTERS WORKING

--- realEstate/views.py
+++ realEstate/views.py
@@ -9,8 +9,16 @@ from realEstate.forms.filter_form import FilterForm
 def index(request):
     country_list = Address.objects.distinct('country')
     type_list = Property.objects.distinct('type')
-    if 'search_filter' in request.GET:
-        search_filter = request.GET['search_filter']
+    if request.is_ajax():
+        #search_filter = request.GET['search_filter']
+        country = request.GET['country_field']
+        price_from = request.GET['price_from_field']
+        price_to = request.GET['price_to_field']
+        size_to = request.GET['size_to_field']
+        size_from = request.GET['size_from_field']
+        rooms_from = request.GET['rooms_from_field']
+        rooms_to = request.GET['rooms_to_field']
+        prop_type = request.GET['type_field']
         properties = [{
             'id':  x.id,
             'name': x.name,
@@ -38,69 +46,21 @@ def index(request):
                 'apartmentNumber': x.address.apartmentNumber,
             },
             'firstImage': ('' if x.propertyattribute_set else x.propertyimage_set.first().image)
-        } for x in Property.objects.filter(name__contains=search_filter)]
-        return JsonResponse({'data': properties})
-    if 'filters' in request.GET:
-        filters = request.GET['filters']
-        # country = request.GET['country_field']
-        # price_from = request.GET['price_from_field']
-        # price_to = request.GET['price_to_field']
-        # size_from = request.GET['size_from_field']
-        # size_to = request.GET['size_to_field']
-        # rooms_from = request.GET['rooms_from_field']
-        # rooms_to = request.GET['rooms_to_field']
-        # prop_type = request.GET['type_field']
-        # if price_from > price_to:
-        #     price_temp = price_from
-        #     price_from = price_to
-        #     price_to = price_temp
-        # if size_from > size_to:
-        #     size_temp = size_from
-        #     size_from = size_to
-        #     size_to = size_temp
-        # if rooms_from > rooms_to:
-        #     rooms_temp = rooms_from
-        #     rooms_from = rooms_to
-        #     rooms_to = rooms_temp
-        properties = [{
-            'id': x.id,
-            'name': x.name,
-            'description': x.description,
-            'type': x.type,
-            'price': x.price,
-            'nrBedrooms': x.nrBedrooms,
-            'nrBathrooms': x.nrBathrooms,
-            'squareMeters': x.squareMeters,
-            'constructionYear': x.constructionYear,
-            'dateCreated': x.dateCreated,
-            'sold': x.sold,
-            'seller': {
-                'name': x.seller.first_name + ' ' + x.seller.last_name,
-                'email': x.seller.email,
-                'phone': x.seller.profile.phone,
-            },
-            'address': {
-                'country': x.address.country,
-                'municipality': x.address.municipality,
-                'city': x.address.city,
-                'postCode': x.address.postCode,
-                'streetName': x.address.streetName,
-                'houseNumber': x.address.houseNumber,
-                'apartmentNumber': x.address.apartmentNumber,
-            },
-            'firstImage': ('' if x.propertyattribute_set else x.propertyimage_set.first().image)
         } for x in Property.objects.filter(
-            address__country__contains=filters
-            # price__gte=price_from,
-            # price__lte=price_to
-            # squareMeters__gte=size_from,
-            # squareMeters__lte=size_to,
-            # nrBedrooms__gte=rooms_from,
-            # nrBedrooms__lte=rooms_to,
-            # type__contains=prop_type
+            #name__contains=search_filter,
+            address__country__contains=country,
+            price__gte=price_from,
+            price__lte=price_to,
+            squareMeters__gte=size_from,
+            squareMeters__lte=size_to,
+            nrBedrooms__gte=rooms_from,
+            nrBedrooms__lte=rooms_to,
+            type__contains=prop_type
 
         )]
         return JsonResponse({'data': properties})
+
+
     context = {'properties': Property.objects.order_by('name'), "propertiesNav": "active", 'country_list': country_list,
                'type_list': type_list}
     return render(request, 'realEstate/index.html', context)
