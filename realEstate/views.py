e3a839f68f779daf3387d7a0d800a1b17cab7881
Haraldur <haraldurloki@gmail.com>
2019-05-13 16:04:33 +0000

    finished filter list

--- realEstate/views.py
+++ realEstate/views.py
@@ -1,16 +1,19 @@
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from realEstate.models import Property, PropertyAttribute, Attribute, Address
-from realEstate.forms.filter_form import FilterForm
+
 
 
 # Create your views here.
 
 def index(request):
     country_list = Address.objects.distinct('country')
+    municipality_list = Address.objects.distinct('municipality')
+    city_list = Address.objects.distinct('city')
+    postcode_list = Address.objects.distinct('postCode')
     type_list = Property.objects.distinct('type')
+    year_built_list = Property.objects.distinct('constructionYear')
     if request.is_ajax():
         filters = request.GET
         properties = [{
@@ -44,19 +47,30 @@ def index(request):
         } for x in Property.objects.filter(
             name__contains=filters.get('search_box'),
             address__country__contains=filters.get('country'),
+            address__municipality__contains=filters.get('municipality'),
+            address__city__contains=filters.get('city'),
+            address__postCode__contains=filters.get('postcode'),
             price__gte=filters.get('price_from'),
             price__lte=filters.get('price_to'),
             squareMeters__gte=filters.get('size_from'),
             squareMeters__lte=filters.get('size_to'),
-            nrBedrooms__gte=filters.get('rooms_from'),
-            nrBedrooms__lte=filters.get('rooms_to'),
+            nrBedrooms__gte=filters.get('bedrooms_from'),
+            nrBedrooms__lte=filters.get('bedrooms_to'),
+            nrBathrooms__gte=filters.get('bathrooms_from'),
+            nrBathrooms__lte=filters.get('bathrooms_to'),
+            constructionYear__gte=filters.get('year_built_from'),
+            constructionYear__lte=filters.get('year_built_to'),
             type__contains=filters.get('type')).order_by(request.GET.get('order'))]
         return JsonResponse({'data': properties})
 
     context = {'properties': Property.objects.order_by('name'),
                'propertiesNav': 'active',
                'country_list': country_list,
-               'type_list': type_list}
+               'municipality_list': municipality_list,
+               'city_list': city_list,
+               'postcode_list': postcode_list,
+               'type_list': type_list,
+               'year_built_list': year_built_list}
     return render(request, 'realEstate/index.html', context)
 
 
