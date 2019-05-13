2a05cbc9a79bd51723715bc70ba83cee6ba5a9c9
egilltor17 <egilltor17@ru.is>
2019-05-13 20:47:42 +0000

    Updated images in edit property

--- realEstate/views.py
+++ realEstate/views.py
@@ -1,18 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.forms import modelformset_factory
+from django.forms import modelformset_factory, inlineformset_factory
 from django.http import JsonResponse
-from django.shortcuts import render, redirect, reverse, get_list_or_404, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from datetime import datetime
 
-from realEstate.forms.property_form import AddressForm, PropertyForm, PropertyImagesForm
+from realEstate.forms.property_form import AddressForm, PropertyForm
 from realEstate.models import Property, PropertyAttribute, PropertyImage, Attribute, Address
-from user.forms.recently_viewed_form import RecentlyViewedForm
 from user.models import RecentlyViewed
 
 # Create your views here.
-
-
 
 
 def index(request):
@@ -82,6 +79,7 @@ def property_details(request, prop_id):
             'attributes': Attribute.objects.order_by('description')
         })
 
+
 @login_required
 def create(request):
     images_form_set = modelformset_factory(PropertyImage, fields=('image',), extra=5)
@@ -117,37 +115,31 @@ def create(request):
 @login_required
 def update(request, prop_id):
     property_instance = Property.objects.get(pk=prop_id)
+    images_form_set = inlineformset_factory(Property, PropertyImage, fields=('image',))
     if request.user.id != property_instance.seller.id:
         print('Seller id: ' + property_instance.seller.id + '\nUser id: ' + request.user.id)
         return redirect(reverse('user-profile'))
 
-    property_form = PropertyForm(instance=property_instance)
-    address_form = AddressForm(instance=property_instance.address)
-
     if request.method == 'POST':
         property_form = PropertyForm(data=request.POST, instance=property_instance)
         address_form = AddressForm(data=request.POST, instance=property_instance.address)
+        image_form = images_form_set(data=request.POST, instance=property_instance)
 
-        #image_form = PropertyImagesForm(data=request.POST)
-        if property_form.is_valid() and address_form.is_valid():
+        if property_form.is_valid() and address_form.is_valid() and image_form.is_valid():
             prop = property_form.save(commit=False)
             prop.address = address_form.save()
             prop.save()
 
-            #image = image_form.save(commit=False)
-            #image.property_id = prop.id
-            #image.save()
+            image_form.save()
             return redirect(reverse('user-profile'))
         else:
             context = { 'pk': prop_id,
                         'address_form': address_form,
                         'property_form': property_form,
-                        #'image_form': image_form,
-                        }
+                        'image_form': image_form, }
             return render(request, 'realEstate/edit-property.html', context)
     context = {'pk': prop_id,
-               'address_form': address_form,
-               'property_form': property_form,
-               # 'image_form': image_form,
-               }
+               'address_form': AddressForm(instance=property_instance.address),
+               'property_form': PropertyForm(instance=property_instance),
+               'image_form': images_form_set(instance=property_instance), }
     return render(request, 'realEstate/edit-property.html', context)
