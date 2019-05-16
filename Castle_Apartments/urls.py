cbaf71ef5a607b8c71a5ab668b529aac062fc252
egilltor17 <egilltor17@ru.is>
2019-05-16 11:05:32 +0000

    Attribute filtering works

--- Castle_Apartments/urls.py
+++ Castle_Apartments/urls.py
@@ -17,13 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', include('miscellaneous.urls')),
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
     path('property/', include('realEstate.urls')),
+    path(r'.*', RedirectView.as_view(url='property-index'), name='redirect'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
