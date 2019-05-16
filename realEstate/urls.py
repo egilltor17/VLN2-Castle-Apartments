29f3a2500680bf145eccdf93fc79280e3482376a
egilltor17 <egilltor17@ru.is>
2019-05-16 20:03:37 +0000

    naming convention pass

--- realEstate/urls.py
+++ realEstate/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='property-index'),
     path('<int:prop_id>', views.property_details, name='property-details'),
-    path('add-property', views.create_property, name='add_property'),
-    path('edit-property/<int:prop_id>', views.update_property, name='edit_property'),
-    path('unfavorite-property/<int:prop_id>', views.unfavorite_property, name="unfavorite_property"),
-    path('favorite-property/<int:prop_id>', views.favorite_property, name="favorite_property"),
+    path('add-property', views.create_property, name='add-property'),
+    path('edit-property/<int:prop_id>', views.update_property, name='edit-property'),
+    path('unfavorite-property/<int:prop_id>', views.unfavorite_property, name="unfavorite-property"),
+    path('favorite-property/<int:prop_id>', views.favorite_property, name="favorite-property"),
 ]
--- user/views.py
+++ user/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect, reverse
 from django.db.models import Case, When
 
-from realEstate.models import Property, Address
+from realEstate.models import Property
 from user.models import Profile, RecentlyViewed, Favorites, Purchase
 from user.forms.registration_form import ProfileForm, UserForm
 # Create your views here.
@@ -14,30 +14,26 @@ from user.forms.registration_form import ProfileForm, UserForm
 def profile(request):
     # Get all recently viewed properties for the user logged in
     pks = [rec.property_id for rec in RecentlyViewed.objects.filter(user_id=request.user.id).order_by('-timestamp')]
-    # To hold the order by timestamp
-    preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(pks)])
+    preserved = Case(*[When(pk=pk, then=pos) for pos, pk in enumerate(pks)])  # To hold the order by timestamp
     recently_viewed_properties = Property.objects.filter(pk__in=pks).order_by(preserved)[:10]
 
     # Get all favorites for the user logged in
     pks2 = [fav.property_id for fav in Favorites.objects.filter(user_id=request.user.id)]
-    favorites = Property.objects.filter(pk__in=pks2)
+    favorite_properties = Property.objects.filter(pk__in=pks2)
 
-    # Get all purchased properties for the user logged in
-    purchased_properties = Purchase.objects.filter(userInfo_id=request.user.id)
-
-    context = {'properties': Property.objects.filter(seller=request.user).order_by('-dateCreated'),
-               'recently_viewed_properties': recently_viewed_properties,
-               'favorites': favorites,
-               'purchased_properties': purchased_properties}
+    context = {'listings': Property.objects.filter(seller=request.user).order_by('-dateCreated'),
+               'recently_viewed': recently_viewed_properties,
+               'favorites': favorite_properties,
+               'purchased': Purchase.objects.filter(userInfo_id=request.user.id)}
     return render(request, 'user/profile.html', context)
 
 
 def seller_profile(request, user_id):
-    if request.user.id == user_id:
+    if request.user.id == user_id:  # Seller is redirected to his own profile
         return redirect('user-profile')
-    context = {'properties': Property.objects.filter(seller__pk=user_id).order_by('-dateCreated'),
+    context = {'listings': Property.objects.filter(seller__pk=user_id).order_by('-dateCreated'),
                'seller': User.objects.get(pk=user_id)}
-    return render(request, 'user/sellerProfile.html', context)
+    return render(request, 'user/seller-profile.html', context)
 
 
 @login_required
@@ -47,6 +43,7 @@ def edit_profile(request):
         profile_form = ProfileForm(instance=profile_instance, data=request.POST, files=request.FILES)
         user_form = UserForm(instance=request.user, data=request.POST)
         if profile_form.is_valid() and user_form.is_valid():
+            # Saving to the database:
             prof = profile_form.save(commit=False)
             prof.profileImage = (prof.profileImage if prof.profileImage else 'profileImages/user.png')
             prof.save()
@@ -56,11 +53,13 @@ def edit_profile(request):
             login(request, new_user)
             return redirect('user-profile')
         else:
+            # The user reenters invalid information.
             context = {'profile_form': profile_form, 'user_form': user_form}
             return render(request, 'user/edit-profile.html', context)
     else:
+        # The page is initially loaded with blank a form.
         context = {'profile_form': ProfileForm(instance=profile_instance),
-                   'user_form': UserForm(instance=request.user),}
+                   'user_form': UserForm(instance=request.user)}
         return render(request, 'user/edit-profile.html', context)
 
 
@@ -69,6 +68,7 @@ def register(request):
         profile_form = ProfileForm(data=request.POST, files=request.FILES)
         user_form = UserForm(data=request.POST)
         if profile_form.is_valid() and user_form.is_valid():
+            # Saving to the database:
             prof = profile_form.save(commit=False)
             prof.profileImage = (prof.profileImage if prof.profileImage else 'profileImages/user.png')
             prof.user = user_form.save()
@@ -78,8 +78,10 @@ def register(request):
             login(request, new_user)
             return redirect(reverse('user-profile'))
         else:
+            # The user reenters invalid information.
             context = {'profile_form': profile_form, 'user_form': user_form}
             return render(request, 'user/register.html', context)
     else:
+        # The page is initially loaded with blank a form.
         context = {'profile_form': ProfileForm(), 'user_form': UserForm()}
         return render(request, 'user/register.html', context)
